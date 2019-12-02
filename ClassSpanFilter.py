08ca629c4fa75dfa88f31a4cb4090af2684da0bd
Gnaiqing <guannq@pku.edu.cn>
2019-12-02 22:31:54 +0800

    add the record document of lecture

--- ClassSpanFilter.py
+++ ClassSpanFilter.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 import re
 
+
 # get keywords of class theme
 def extract_keywords(topK_for_textbook=8, topK_for_blog=4):
     jieba.analyse.set_stop_words('stopwords.txt')
@@ -20,9 +21,9 @@ def extract_keywords(topK_for_textbook=8, topK_for_blog=4):
                 keywords[i] += 1
             else:
                 keywords[i] = 1
-    #print(keywords)
-    keywords = dict(sorted(keywords.items(), key=lambda x:x[1], reverse=True))
-    #print(keywords)
+    # print(keywords)
+    keywords = dict(sorted(keywords.items(), key=lambda x: x[1], reverse=True))
+    # print(keywords)
 
     blog = open('blog.txt', 'r', encoding='gbk')
     lines = blog.readlines()
@@ -34,28 +35,31 @@ def extract_keywords(topK_for_textbook=8, topK_for_blog=4):
                 keywords[i] += 1
             else:
                 keywords[i] = 1
-    #print(keywords)
+    # print(keywords)
     keywords = dict(sorted(keywords.items(), key=lambda x: x[1], reverse=True))
-    #print(keywords)
-    
+    # print(keywords)
+
     return keywords
-    
+
+
 # e.g. keywords = {'文档': 10, '文本': 10, '聚类': 10}, the value is the extracted frequency of the keywords, which can be interpreted as importance
 keywords = extract_keywords(topK_for_textbook=8, topK_for_blog=4)
 keywords_list = []
-for k,v in keywords.items():
+for k, v in keywords.items():
     keywords_list.append(k.lower())
 print("keywords_list:", keywords_list)
 
 # get input sentences of tapescripts
 # e.g. sentences = ["这是一个句子。","那是一个句子。"]
-paragraph = "生活对我们KNN来说都不容易！我们必须努力，最重要的是我们必须相信K-Means。 \
-我们必须相信，我们每个KNN都能够做得很好，而且，当我们发现这是什么时，我们必须努力工作，直到我们成功。"
-tmp_sents = re.split('(。|！|\!|\.|？|\?)',paragraph)
+# paragraph = "生活对我们KNN来说都不容易！我们必须努力，最重要的是我们必须相信K-Means。 \
+# 我们必须相信，我们每个KNN都能够做得很好，而且，当我们发现这是什么时，我们必须努力工作，直到我们成功。"
+f = open("record.txt", "r", encoding="UTF-8")
+paragraph = f.read()
+tmp_sents = re.split('(。|！|\!|\.|？|\?)', paragraph)
 sentences = []
-sentence_num = int(len(tmp_sents)/2)
+sentence_num = int(len(tmp_sents) / 2)
 for i in range(sentence_num):
-    sent = tmp_sents[2*i] + tmp_sents[2*i+1]
+    sent = tmp_sents[2 * i] + tmp_sents[2 * i + 1]
     sentences.append(sent)
 # print("sentences:", sentences)
 
@@ -101,7 +105,23 @@ for i in range(sentence_num):
     score = 0
     for j in range(appeared_keywords_num):
         score = score + tfidf_array[i][keywords_index[j]]
-    sentence_scores.append(score)
-print("scores:", sentence_scores)
+    sentence_scores.append((i, score))
+
+
+# print("scores:", sentence_scores)
 
 # display the result in various ways
+def takeSecond(elem):
+    return elem[1]
+
+
+sentence_scores.sort(key=takeSecond)
+k = 10
+print("Top ", k, "Relevant Sentences:")
+for i in range(k):
+    print(sentences[sentence_scores[-i-1][0]])
+
+print("Top ",k, "Irrelevant Sentences:")
+for i in range(k):
+    print(sentences[sentence_scores[i][0]])
+
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is the homework of Modern IR
 textbook.txt is the introduction of chapter 13-18 in the Chinese version of IIR.
 
 blog.txt is the modified version of blog https://blog.csdn.net/riverflowrand/article/details/50991886
+
+record.txt is the record of lecture given by Professor Sherlock Holmes
